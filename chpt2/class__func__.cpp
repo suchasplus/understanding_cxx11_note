c2b60e2895897e660e0071698d179f8f323dae71
suchasplus <suchasplus@gmail.com>
2015-08-24 19:00:48 -0400

    2-3 class with __func__ in ctor

---
+++ chpt2/class__func__.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+struct TestStruct {
+    TestStruct () : name(__func__) {}
+    const char *name;
+};
+
+int main(int argc, const char *argv[])
+{
+    TestStruct ts;
+    cout << ts.name << endl;
+    return 0;
+}
