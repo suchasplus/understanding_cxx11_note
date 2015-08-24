3c8d9d73a3608ab39d189a62ff9308fae3ed3627
suchasplus <suchasplus@gmail.com>
2015-08-24 19:02:47 -0400

    add func fail

--- chpt2/class__func__.cpp
+++ chpt2/class__func__.cpp
@@ -5,6 +5,8 @@ using namespace std;
 struct TestStruct {
     TestStruct () : name(__func__) {}
     const char *name;
+    // warning: ‘__func__’ is not defined outside of function scope [enabled by default]
+    //void FuncFail ( string func_name = __func__) {};
 };
 
 int main(int argc, const char *argv[])
