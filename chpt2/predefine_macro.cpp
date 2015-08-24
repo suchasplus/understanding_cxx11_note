9b71b60d89babf9e5022447c3f07fff4d59c9224
suchasplus <suchasplus@gmail.com>
2015-08-24 18:47:30 -0400

    add predefine_macro in 2-1

---
+++ chpt2/predefine_macro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    cout << "std clib : " << __STDC_HOSTED__ << endl;
+    cout << "std c : " << __STDC__ << endl;
+    // __STDC_VERSION__ not impl
+    //cout << "c std version : " << __STDC_VERSION__ << endl;
+    cout << "ISO/IEC : " << __STDC_ISO_10646__ << endl;
+    return 0;
+}
