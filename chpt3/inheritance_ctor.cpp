57764d59feb1a8b1d97e640b62efa853942803bf
suchasplus <suchasplus@gmail.com>
2015-08-24 22:28:35 -0400

    initial chapter3

---
+++ chpt3/inheritance_ctor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct A {
+    A ( int i ) {
+        std::cout << "A ctor : int i = " << i << std::endl;
+    }
+};
+
+struct B : A {
+    B (int i ) : A (i) , d(i) {
+        std::cout << "B ctor : int i^2 = " << d * d << std::endl;
+    }
+
+    int d;
+};
+
+int main(int argc, const char *argv[])
+{
+    A a(10);
+    B b(20);
+    return 0;
+}
